747047b18a0f6c21a630328695b3e2619892267f
MauroSei <mauro.seimandi64@gmail.com>
2023-11-30 01:47:58 -0300

    uséPOO

--- datos.py
+++ datos.py
@@ -4,23 +4,25 @@ import random
 with open('Animales.txt', 'r') as archivo1:
     animal = archivo1.readlines()
     a = [linea.strip() for linea in animal]
-
 archivo1.close()
-
 with open('Colores.txt', 'r') as archivo2:
     color = archivo2.readlines()
     b = [linea.strip() for linea in color]
-
 archivo2.close()
 
 
+class Player():
+    def __init__(self):
+        self.nombre=''
+        self.puntaje=0
 
-def generar_nombre():
-    print(a.choise+b.choise)
+    def generar_nombre(self,nombre):
+        nombre = random.choice(a)+' '+random.choice(b)
+        return nombre
 
-def guardar_puntaje():
-    pass
+    def guardar_puntaje(self,puntaje):
+        pass
 
-print(b)
+    def mensaje():
+        pass
 
-#generar_nombre()
