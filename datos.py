74db9f30c1b3db07bc7a72ef5c7f6836bc34c320
MauroSei <mauro.seimandi64@gmail.com>
2023-11-30 01:24:04 -0300

    eliminaciónDeTildes

--- datos.py
+++ datos.py
@@ -1,11 +1,19 @@
 import random
 
-archivo1=open('Animales.txt', 'r')
-a=[archivo1.readline()]
+
+with open('Animales.txt', 'r') as archivo1:
+    animal = archivo1.readlines()
+    a = [linea.strip() for linea in animal]
+
 archivo1.close()
-archivo2=open('Colores.txt', 'r')
-b=[archivo2.readline()]
+
+with open('Colores.txt', 'r') as archivo2:
+    color = archivo2.readlines()
+    b = [linea.strip() for linea in color]
+
 archivo2.close()
+
+
 
 def generar_nombre():
     print(a.choise+b.choise)
@@ -13,4 +21,6 @@ def generar_nombre():
 def guardar_puntaje():
     pass
 
-generar_nombre()
+print(b)
+
+#generar_nombre()
